b303d4924e91d8bff7ff979d7df091b29335fe7e
Alius Petraska <alius.petraska@gmail.com>
2017-05-10 14:09:59 +0300

    initial commit;

---
+++ parkingbot/Controllers/LaisvaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using parkingbot.Models;
+
+namespace parkingbot.Controllers
+{
+    [Route("api/[controller]")]
+    public class LaisvaController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Works!";
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] string value)
+        {
+            var result = new Response
+            {
+                response_type = "ephemeral",
+                text = "text",
+                attachments = new List<Attachment>
+                {
+                    new Attachment
+                    {
+                        text = "attachment"
+                    }
+                }
+            };
+
+            return Json(result);
+        }
+    }
+}
---
+++ parkingbot/Models/Response.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace parkingbot.Models
+{
+    public class Attachment
+    {
+        public string text { get; set; }
+    }
+
+    public class Response
+    {
+        public string response_type { get; set; }
+        public string text { get; set; }
+        public List<Attachment> attachments { get; set; }
+    }
+}
